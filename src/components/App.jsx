d44355f49ab91038b0c6b8259798076632bdf7c7
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2024-02-25 21:59:14 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from 'pages/Home/Home';
 import Tweets from 'pages/Tweets/Tweets';
@@ -9,6 +9,7 @@ const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
